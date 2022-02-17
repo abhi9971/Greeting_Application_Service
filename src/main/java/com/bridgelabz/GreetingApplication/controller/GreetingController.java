f2feb44e0e9777acf25528d83848d466e936a0d0
abhi9971 <abhishekrajawat101gmail.com>
2022-02-17 21:43:38 +0530

    ADDED:service layer

--- src/main/java/com/bridgelabz/GreetingApplication/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/GreetingApplication/controller/GreetingController.java
@@ -1,13 +1,15 @@
 package com.bridgelabz.GreetingApplication.controller;
 
 
+import com.bridgelabz.GreetingApplication.service.GreetingAppService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/start")
 public class GreetingController {
-
-
+    @Autowired
+    GreetingAppService service;
     @GetMapping("/greeting")
     public String greeting(@RequestParam(value = "name",defaultValue = "world") String name) {
         return "Hello..!!! " + name;
